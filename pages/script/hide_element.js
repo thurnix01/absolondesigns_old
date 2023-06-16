9535074fbfe6accb7574fc7a514fc19af4be24fc
Thurnix <98490306+thurnix01@users.noreply.github.com>
2023-06-16 13:54:28 -0500

    Create hide_element.js

---
+++ pages/script/hide_element.js
@@ -0,0 +1,9 @@
+     function toggleElement() {
+          var element = document.querySelector(".component-builder");
+
+          if (element.style.display === "none") {
+            element.style.display = "block";
+          } else {
+            element.style.display = "none";
+          }
+        }
