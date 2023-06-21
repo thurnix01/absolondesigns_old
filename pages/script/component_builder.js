e20be12c05c7879edf39c71d4c61f6853b29680c
Thurnix <98490306+thurnix01@users.noreply.github.com>
2023-06-21 15:16:25 -0500

    Update component_builder.js

--- pages/script/component_builder.js
+++ pages/script/component_builder.js
@@ -285,7 +285,7 @@
             <div style="background-color: #f9f9f9; height: 300px; width: 298px; display: flex;flex-direction: column; justify-content: center;">
                 <div style="padding: 16px; display: flex;flex-direction: column;justify-content: center;">
                 <h3 style="text-align: left; margin:32px 0 16px;">${introBodyTitle2}</h3>
-                <p style="font-family:'UniversLTStd-Cn', sans-serif;font-weight: 500; line-height:1.5rem; font-size: 1rem; text-align: left; margin: 1rem; color: #000;">${imageCopy3}</p>
+                <p style="font-family:'UniversLTStd-Cn', sans-serif;font-weight: 500; line-height:1.5rem; font-size: 1rem; text-align: left; margin: 1rem 0; color: #000;">${imageCopy3}</p>
                 <a href="${buttonHref3}" target="_blank" style="font-family:'UniversLTStd-Cn', sans-serif;font-weight: 500; line-height:1.5rem; font-size: 1rem; text-align: left; margin: 1rem 0; color: #000; text-transform: uppercase;">${buttonLabel3}</a>
                 </div>
             </div>
