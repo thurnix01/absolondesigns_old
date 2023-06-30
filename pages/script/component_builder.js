8cec18aa6ba3a790e297ae9a4ccb8ffdba6eacbd
Thurnix <98490306+thurnix01@users.noreply.github.com>
2023-06-30 15:58:31 -0500

    Update component_builder.js

--- pages/script/component_builder.js
+++ pages/script/component_builder.js
@@ -1,4 +1,4 @@
-   
+      
       function addTemplate() {
         var heroImage = document.getElementById("hero-image").value;
         var introLine = document.getElementById("intro-line").value;
@@ -352,54 +352,3 @@
         }
       }        
         
-
-    </script>
-    
- <!-----------------Button-Alternate---------------------------->
-    
-    
-    <script>
-        function buttonStyle() {
-        var myDiv = document.querySelector("._header-cta");
-
-            if (myDiv.classList.contains("active")) {
-                myDiv.classList.remove("active");
-            } else {
-                myDiv.classList.add("active");
-            }
-        }
-        function buttonStyleColor() {
-            var myDiv = document.querySelector("._header-cta");
-
-            if (myDiv.classList.contains("active")) {
-                myDiv.style.backgroundColor = "#464647";
-            } else {
-                myDiv.style.backgroundColor = "#E04F26";
-                myDiv.style.border = "2.5px solid #E04F26";
-            }
-        }
-
-
-
-
-        function buttonStyle2() {
-        var myDiv = document.querySelector("._body-cta");
-
-            if (myDiv.classList.contains("active")) {
-                myDiv.classList.remove("active");
-            } else {
-                myDiv.classList.add("active");
-            }
-        }
-
-        function buttonStyleColor2() {
-        var myDiv = document.querySelector("._body-cta");
-
-            if (myDiv.classList.contains("active")) {
-                myDiv.style.backgroundColor = "#464647";
-            } else {
-                myDiv.style.backgroundColor = "#E04F26";
-                myDiv.style.border = "2.5px solid #E04F26";
-            }
-        }
-
