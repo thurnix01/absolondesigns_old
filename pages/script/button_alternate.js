18d3d4c3020ea8811f450bf4a500db18720c0df8
Thurnix <98490306+thurnix01@users.noreply.github.com>
2023-07-13 14:06:47 -0500

    Update button_alternate.js

--- pages/script/button_alternate.js
+++ pages/script/button_alternate.js
@@ -1,43 +1,58 @@
-   function buttonStyle() {
-        var myDiv = document.querySelector("._header-cta");
+    function buttonStyle() {
+    var myDiv = document.querySelector(".btn-style");
 
-            if (myDiv.classList.contains("active")) {
-                myDiv.classList.remove("active");
-            } else {
-                myDiv.classList.add("active");
-            }
-        }
-        function buttonStyleColor() {
-            var myDiv = document.querySelector("._header-cta");
+    if (myDiv.classList.contains("active")) {
+      myDiv.style.backgroundColor = "#464647";
+      }  else {
+      myDiv.style.backgroundColor = "#fff";
+      myDiv.style.border = "2.5px solid #333";
+      myDiv.style.color = "#333";    
+    }
+      
 
-            if (myDiv.classList.contains("active")) {
-                myDiv.style.backgroundColor = "#464647";
-            } else {
-                myDiv.style.backgroundColor = "#E04F26";
-                myDiv.style.border = "2.5px solid #E04F26";
-            }
-        }
+  }
+  
+
+    
+   function buttonStyleColor() {
+    var myDiv = document.querySelector(".btn-style");
+
+    if (myDiv.classList.contains("active")) {
+      myDiv.style.backgroundColor = "#464647";
+      }  else {
+      myDiv.style.backgroundColor = "#E04F26";
+      myDiv.style.border = "2.5px solid #E04F26";
+      myDiv.style.color = "#fff"; 
+    }
+  }
 
 
 
 
-        function buttonStyle2() {
-        var myDiv = document.querySelector("._body-cta");
+      function buttonStyle2() {
+    var myDiv = document.querySelector(".btn-style");
 
-            if (myDiv.classList.contains("active")) {
-                myDiv.classList.remove("active");
-            } else {
-                myDiv.classList.add("active");
-            }
-        }
+    if (myDiv.classList.contains("active")) {
+      myDiv.style.backgroundColor = "#464647";
+      }  else {
+      myDiv.style.backgroundColor = "#fff";
+      myDiv.style.border = "2.5px solid #333";
+      myDiv.style.color = "#333";    
+    }
+      
 
-        function buttonStyleColor2() {
-        var myDiv = document.querySelector("._body-cta");
+  }
+  
 
-            if (myDiv.classList.contains("active")) {
-                myDiv.style.backgroundColor = "#464647";
-            } else {
-                myDiv.style.backgroundColor = "#E04F26";
-                myDiv.style.border = "2.5px solid #E04F26";
-            }
-        }
+    
+   function buttonStyleColor2() {
+    var myDiv = document.querySelector(".btn-style");
+
+    if (myDiv.classList.contains("active")) {
+      myDiv.style.backgroundColor = "#464647";
+      }  else {
+      myDiv.style.backgroundColor = "#E04F26";
+      myDiv.style.border = "2.5px solid #E04F26";
+      myDiv.style.color = "#fff"; 
+    }
+  }
