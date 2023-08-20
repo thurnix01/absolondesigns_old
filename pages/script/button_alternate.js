476e7b68ebdbff62699a0758186e4038e2e96a90
Thurnix <98490306+thurnix01@users.noreply.github.com>
2023-08-20 10:19:13 -0500

    Update button_alternate.js

--- pages/script/button_alternate.js
+++ pages/script/button_alternate.js
@@ -26,32 +26,4 @@
     }
   }
     
-    
-        function buttonStyle2() {
-    var myDiv = document.querySelector(".btn-style2");
-
-    if (myDiv.classList.contains("active")) {
-      myDiv.style.backgroundColor = "#464647";
-      }  else {
-      myDiv.style.backgroundColor = "#fff";
-      myDiv.style.border = "2.5px solid #333";
-      myDiv.style.color = "#333";    
-    }
-      
-
-  }
-  
-
-    
-   function buttonStyleColor2() {
-    var myDiv = document.querySelector(".btn-style2");
-
-    if (myDiv.classList.contains("active")) {
-      myDiv.style.backgroundColor = "#464647";
-      }  else {
-      myDiv.style.backgroundColor = "#E04F26";
-      myDiv.style.border = "2.5px solid #E04F26";
-      myDiv.style.color = "#fff"; 
-    }
-  }
-  
+ 
