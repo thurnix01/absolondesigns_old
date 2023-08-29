e2e629ec80a34741b69e06224b1e660c02621d4d
Thurnix <98490306+thurnix01@users.noreply.github.com>
2023-08-29 14:37:02 -0500

    Update component_builder.js

--- pages/script/component_builder.js
+++ pages/script/component_builder.js
@@ -404,16 +404,16 @@
 
             
             template.innerHTML = `
-                               <tr style="padding:0 16px 0; display: block;"  class="_introline">
-                        <td align="center" valign="middle" width="100%" style="width:600px;">
-                                 <!--[if mso]><table width="600" cellpadding="0" cellspacing="0" border="0" align="center"><tr><td style="padding: 16px 32px 16px;"><![endif]-->
 
-                               <p style="margin: 0 32px 0;padding: 16px;color:#000;font:300 16px/1.5  'UniversLTStd-Cn', Helvetica, Arial, Tahoma, sans-serif; position: relative; top: 4px; text-align: inherit;" class="_introline">${imageCopy2} </p>
+                 <tr style="padding:0 16px 0; display: block;"  class="_introline">
+                        <td align="center" valign="middle" width="100%" style="width:600px;">
+                                 <!--[if mso]><table width="600" cellpadding="0" cellspacing="0" border="0" align="center"><tr><td style="padding: 16px 32px 16px; text-align: center;"><![endif]-->
+
+                               <p style="margin: 0 32px 0;padding: 16px;color:#000;font:300 16px/1.5  'UniversLTStd-Cn', Helvetica, Arial, Tahoma, sans-serif; position: relative; top: 4px; text-align: inherit; " class="_introline">${imageCopy2} </p>
                               
                           <!--[if mso]></td></tr></table><![endif]-->
                         </td>
-                </tr>
-                                        
+                </tr>                                        
                     `;
             
             
