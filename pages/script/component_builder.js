5d72fb3844073d2de2b40a38486a71c5352c6606
Thurnix <98490306+thurnix01@users.noreply.github.com>
2023-10-16 11:58:38 -0500

    Update component_builder.js

--- pages/script/component_builder.js
+++ pages/script/component_builder.js
@@ -668,29 +668,38 @@
         var imageCopy4 = document.getElementById("image-copy-4").value;
         var buttonLabel4 = document.getElementById("buttonLabel4").value;
         var buttonHref4 = document.getElementById("buttonHref4").value;      
-        var template = document.createElement("td");
+
+       var template = document.createElement("table");
         template.className = "product"; 
+        template.style.maxWidth = "600px";
+        template.width = "600";
+        template.style.width = "100%";
+        template.style.margin = "0 auto";
+        template.style.height = "auto";
+        template.verticalAlign = "top";
+        template.border = "0";
+        template.cellPadding = "0";
+        template.cellSpacing = "0";
+        template.role = "presentation";
         template.align = "center";
-        template.style.display = "grid";  
-        template.style.width = "100%"; 
-        template.style.margin = "0 auto"; 
-        template.style.maxWidth = "502px"; 
-        template.style.textAlign = "center";   
-        template.style.flexGrow = "1";
-        template.style.flexShrink = "0";   
+ 
          
         template.innerHTML = `
+
+        <tr align="center" style="width: 100%; margin:0 auto; max-width: 502px; text-align: center;">
+
         <a href="${buttonHref4}" target="_blank"><img src="${image4}" alt="" style="width:100%; margin: 0 auto; display: flex; height: auto; object-fit: contain;" border="0" width="300" id="imageAlt2"></a>
         <p style="margin: 0 auto 12px; width: 100%; font-family: 'UniversLTStd-Cn', Helvetica, Arial, Tahoma, sans-serif; text-align: center; font-size:17px;font-weight: normal; color:#000; height:auto; padding-top: 16px;" >${imageCopy4}</p>
 
             <!--[if mso]>
-                <v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="${buttonHref4}" target="_blank" style="v-text-anchor:middle;width:220px;height: 52px;text-transform: uppercase;" arcsize="11.11111111111111%" strokecolor="#464647" fillcolor="#464647">
+                <v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="${buttonHref4}" target="_blank" style="v-text-anchor:middle;width:260px;height: 52px;text-transform: uppercase;" arcsize="11.11111111111111%" strokecolor="#464647" fillcolor="#464647">
                     <w:anchorlock />
                     <center style="color:#ffffff;font-family:'UniversLTStd-Cn','Roboto Condensed','', sans-serif; font-size:16px;font-weight:bold;text-transform: uppercase !important;" class="_cta_btn"><span class="button-text">${buttonLabel4}</span></center>
                 </v:roundrect>
                 <![endif]-->
                 
-                <a href="${buttonHref4}" target="_blank" style="background-color:#464647;border:1px solid #464647;border-radius:5px;color:#ffffff;display:block;font-family:'UniversLTStd-Cn','Roboto Condensed','', sans-serif; font-size: 1rem;font-weight:normal;line-height:56px;text-align:center;text-decoration:none;text-transform: uppercase !important;width:220px;height: 52px;margin:0 auto;-webkit-text-size-adjust:none;mso-hide:all;" class="_cta_btn remove-btn" ><span class="button-text">${buttonLabel4}</span></a>
+                <a href="${buttonHref4}" target="_blank" style="background-color:#464647;border:1px solid #464647;border-radius:5px;color:#ffffff;display:table;font-family:'UniversLTStd-Cn','Roboto Condensed','', sans-serif; font-size: 1rem;font-weight:normal;line-height:56px;text-align:center;text-decoration:none;text-transform: uppercase !important;min-width:204px;width:auto;margin:0 auto;padding: 5px 16px 0;height: 52px;-webkit-text-size-adjust:none;mso-hide:all;" class="_cta_btn remove-btn" ><span class="button-text">${buttonLabel4}</span></a><br><br>
+        </tr>
         `;  
      
         container.appendChild(template);
