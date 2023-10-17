64a50591b7db873e44a7d595c01dd68e33ac98ec
Thurnix <98490306+thurnix01@users.noreply.github.com>
2023-10-17 13:17:51 -0500

    Update component_builder.js

--- pages/script/component_builder.js
+++ pages/script/component_builder.js
@@ -683,18 +683,19 @@
         template.align = "center";
  
          
-        template.innerHTML = `
-
+         template.innerHTML = `
 
         <tr style="vertical-align: top">
-            <td style="word-break: break-word;border-collapse: collapse !important;vertical-align: top; margin: 0;padding:0 ;">
+            <td style="word-break: break-word;border-collapse: collapse !important;vertical-align: top; margin: 0;padding:0 16px;">
                 <!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0" align="center"><tr><td align="center" style="background-color: #FFFFFF;margin: 0;padding:0;"><![endif]-->
 
             <div align="center" style="margin: 0;padding:0; border-radius: 8px;  overflow: hidden;">
                     <!--[if mso]><table width="536" cellpadding="0" cellspacing="0" border="0" align="center"><tr style="line-height:0px;line-height:0px;"><td style="padding-right: 0px; padding-left: 0px; width:536px; margin: 0 auto;" align="center"><![endif]-->
 
-        <a href="${buttonHref4}" target="_blank"><img align="center" border="0" src="${image4}" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: block !important;border: none; float: none;width: 100%;max-width: 536px;  object-fit: contain; object-position: center;mix-blend-mode: darken;" width="536" id="imageAlt2"></a>
-        <p style="margin: 0 auto 12px; width: 100%; max-width: 536px;font-family: 'UniversLTStd-Cn', Helvetica, Arial, Tahoma, sans-serif; text-align: center; font-size:17px;font-weight: normal; color:#000; height:auto; padding-top: 16px;" >${imageCopy4}</p>
+        <a href="${buttonHref4}" target="_blank"><img align="center" border="0" src="${image4}" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: block !important;border: none; float: none;width: 100%;max-width: 502px;  object-fit: contain; object-position: center; border-radius: 8px; overflow: hidden;" width="536" id="imageAlt2"></a>
+
+
+        <p style="margin: 0 auto 12px; width: 100%; font-family: 'UniversLTStd-Cn', Helvetica, Arial, Tahoma, sans-serif; text-align: center; font-size:17px;font-weight: normal; color:#000; height:auto; padding-top: 16px;" >${imageCopy4}</p>
 
             <!--[if mso]>
                 <v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="${buttonHref4}" target="_blank" style="v-text-anchor:middle;width:260px;height: 52px;text-transform: uppercase;" arcsize="11.11111111111111%" strokecolor="#464647" fillcolor="#464647">
