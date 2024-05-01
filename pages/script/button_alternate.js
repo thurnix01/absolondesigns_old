6e1a25b31f2e6debd260260f22510be79fc08e65
Thurnix <98490306+thurnix01@users.noreply.github.com>
2024-05-01 16:07:24 -0700

    Update button_alternate.js

--- pages/script/button_alternate.js
+++ pages/script/button_alternate.js
@@ -25,5 +25,29 @@
       myDiv.style.color = "#fff"; 
     }
   }
+
+    
+ <!-----------------Background-Color-Alternate----------------->
+   
+    <script>   
+        
+        
+        function backColor() {
+    var myDiv = document.querySelectorAll('._header-section, ._header-cta');
+    
+    for (var i = 0; i < myDiv.length; i++) {
+        if (myDiv[i].classList.contains('_header-section')) {
+            myDiv[i].style.backgroundColor = '#000';
+            myDiv[i].style.color = '#fff';
+        } else if (myDiv[i].classList.contains('_header-cta')) {
+            myDiv[i].style.backgroundColor = '#000';
+             myDiv[i].style.color = '#fff';
+            myDiv[i].style.paddingBottom = '32px';
+        }
+    }
+}
+  
+ </script>
+    
     
  
